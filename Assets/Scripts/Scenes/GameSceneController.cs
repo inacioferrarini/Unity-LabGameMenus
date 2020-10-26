1ddd5b53bb64d79518e9b8426a747b0a8a1aac59
Inácio Ferrarini <jose.ferrarini@solutis.com.br>
2020-10-26 18:23:25 -0300

    Adds GameSceneController

---
+++ Assets/Scripts/Scenes/GameSceneController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LabGameMenus.Scenes {
+
+	public class GameSceneController: MonoBehaviour {
+
+		#region Menu Event Handling
+
+		public void HandlePlayerDiedAction() {
+			Debug.Log("Player Died");
+		}
+
+		public void HandleShowMenuAction() {
+			Debug.Log("Show Menu");
+		}
+
+		#endregion
+
+	}
+
+}
